cf8c7af0caa1f66e3dff2cd75dc5d005ed0b0fb9
Quoc Duong Bui <bui.qd@canida.io>
2024-08-07 11:24:34 +0200

    Add prop docs to RequestHandler type

--- src/types/render.d.ts
+++ src/types/render.d.ts
@@ -6,15 +6,18 @@ export type RequestFn = (
 ) => void;
 
 export type RequestHandler = {
+  /** `Source` request handler */
   [ResourceKind.Source]: RequestFn,
+  /** `Tile` request handler */
   [ResourceKind.Tile]: RequestFn,
+  /** `Glyphs` request handler */
   [ResourceKind.Glyphs]: RequestFn,
+  /** `SpriteImage` request handler */
   [ResourceKind.SpriteImage]: RequestFn,
+  /** `SpriteJSON` request handler */
   [ResourceKind.SpriteJSON]: RequestFn,
+  /** DO NOT USE! Artifact for handling `ImageSource` requests. May be removed soon.*/
   7: RequestFn, // remaining artifact after refactoring, probably only existed in mapbox and not maplibre
 }
 
 export type PartialRequestHandler = Partial<Omit<RequestHandler, 7>> // removed 7 because it should not be used anymore outside of the default request handler
-
-
-
